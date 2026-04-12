b6432d09ada709619a981d2125e3b69702c40e8c
simonpunk <simonpunk2016@gmail.com>
2026-04-12 18:18:57 +0800

    ksu_module_susfs: Update boot-completed.sh template
    - Fix some typos

--- ksu_module_susfs/boot-completed.sh
+++ ksu_module_susfs/boot-completed.sh
@@ -22,9 +22,9 @@ cat <<EOF >/dev/null
 until [ -d "/sdcard/Android" ]; do sleep 3; done
 
 ## Remove the '..5.u.S' leftover ##
-## THe reason why this sus file is created is because users have grant the WRITE_EXTERNAL_STORAGE permission for the apps that detecting sus files in /sdcard, or in /sdcard/Android/data where the apps are exploiting the unicode bugs to create files arbitrary.
-## susfs redirects the sus path to a supposed not-existing path named '..5.u.S', and this is the only way to settle the cross check of returned errno from various syscalls, but one disadvantage is that if the path itself can be written/created by the app (WRITE_EXTERNAL_STORAGE granted), then it is futile to hide it, but at least here we automatically delete them on each boot.
-## The best practise is to revoke WRITE_EXTERNAL_STORAGE permission for all third party apps. 
+## THe reason why this sus file is created is because users have grant the MANAGE_EXTERNAL_STORAGE permission for the apps that detecting sus files in /sdcard, or in /sdcard/Android/data where the apps are exploiting the unicode bugs to create files arbitrary.
+## susfs redirects the sus path to a supposed not-existing path named '..5.u.S', and this is the only way to settle the cross check of returned errno from various syscalls, but one disadvantage is that if the path itself can be written/created by the app (MANAGE_EXTERNAL_STORAGE granted), then it is futile to hide it, but at least here we automatically delete them on each boot.
+## The best practise is to revoke MANAGE_EXTERNAL_STORAGE permission for all third party apps.
 
 [ -e "/sdcard/..5.u.S" ] && rm -rf "/sdcard/..5.u.S"
 [ -e "/sdcard/Android/data/..5.u.S" ] && rm -rf "/sdcard/Android/data/..5.u.S"
