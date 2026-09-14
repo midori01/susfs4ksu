d141930ac14fc808d625bb4f1680bdc145a8ce10
midori01 <lv@lvlv.lv>
2026-09-14 13:22:13 +0900

    kernel: Adapt susfs.c and susfs_def.h for Linux 5.4
    Signed-off-by: midori01 <lv@lvlv.lv>

--- kernel_patches/fs/susfs.c
+++ kernel_patches/fs/susfs.c
@@ -1435,9 +1435,17 @@ static int watch_one_dir(struct watch_dir *wd)
  * synchronize_srcu on the same SRCU struct, causing a permanent deadlock).
  * Cleanup is deferred to a delayed_work that runs outside the SRCU context.
  */
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)
 static int susfs_handle_sdcard_inode_event(struct fsnotify_mark *mark, u32 mask,
 											struct inode *inode, struct inode *dir,
 											const struct qstr *file_name, u32 cookie)
+#else
+static int susfs_handle_sdcard_inode_event(struct fsnotify_group *group,
+			    struct inode *inode,
+			    u32 mask, const void *data, int data_type,
+			    const struct qstr *file_name, u32 cookie,
+			    struct fsnotify_iter_info *iter_info)
+#endif
 {
 	if (!file_name || file_name->len != 7 ||
 	    memcmp(file_name->name, "Android", 7))
@@ -1453,7 +1461,11 @@ static int susfs_handle_sdcard_inode_event(struct fsnotify_mark *mark, u32 mask,
 }
 
 static const struct fsnotify_ops fsnotify_ops = {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)
 	.handle_inode_event = susfs_handle_sdcard_inode_event,
+#else
+	.handle_event = susfs_handle_sdcard_inode_event,
+#endif
 };
 
 static int add_mark_on_inode(struct inode *inode, u32 mask,
